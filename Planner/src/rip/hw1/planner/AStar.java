f9ed333ded498285a2c757b7b9f89c2595c41775
Sagarsaurus <sagrsccr@gmail.com>
2014-10-04 03:34:08 -0400

    Fixed silly visited list issue

--- Planner/src/rip/hw1/planner/AStar.java
+++ Planner/src/rip/hw1/planner/AStar.java
@@ -1,5 +1,6 @@
 package rip.hw1.planner;
 import java.util.PriorityQueue;
+import java.util.LinkedList;
 /**
  * Created by ajmalkunnummal on 10/3/14.
  */
@@ -8,6 +9,7 @@ public class AStar implements Planner {
     @Override
     public Object[] run(State start, State end, Heuristic heuristic) {
         Priority<State> pq = new PriorityQueue<State>();
+        LinkedList<State> visited = new LinkedList<State>();
         pq.add(start);
         while(!goalReached || !pq.isEmpty()) {
             State current = pq.poll();
@@ -16,9 +18,12 @@ public class AStar implements Planner {
             }
 
             else {
+                visited.add(current);
                 for(State s : current.neighbours()) {
-                    s.setTentativeDistance(s.getActions.length + heuristic.estimate(s,end));
-                    pq.add(s);
+                    if(!visited.contains(s)) {
+                        s.setTentativeDistance(s.getActions.length + heuristic.estimate(s,end));
+                        pq.add(s);
+                    }
                 }
             }
         }
