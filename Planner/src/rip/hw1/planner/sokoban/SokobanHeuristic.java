48b79c9834534fd93be0d6828f56ba46c0d6c5c1
pyrollyr <rstauffer3@gatech.edu>
2014-10-04 17:57:28 -0400

    Implemented SokobanHeuristic
    Should be a simple manhattan distance between current box location to
    goal box location.

--- Planner/src/rip/hw1/planner/sokoban/SokobanHeuristic.java
+++ Planner/src/rip/hw1/planner/sokoban/SokobanHeuristic.java
@@ -2,6 +2,7 @@ package rip.hw1.planner.sokoban;
 
 import rip.hw1.planner.Heuristic;
 import rip.hw1.planner.State;
+import static java.lang.Math.*;
 
 /**
  * Created by ajmalkunnummal on 10/3/14.
@@ -9,6 +10,12 @@ import rip.hw1.planner.State;
 public class SokobanHeuristic implements Heuristic {
     @Override
     public double estimate(State start, State goal) {
-        return 0;
+    	double sum=0;
+        for(i=0;i<start.boxes.length;i++){
+        	sum+=Math.abs(start.boxes[i].x-goal.boxes[i].x)
+        	sum+=Math.abs(start.boxes[i].y-goal.boxes[i].y)
+        }
+        return sum;
+        
     }
 }
