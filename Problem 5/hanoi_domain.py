825246012c81771179e8dc153e62866253930e12
Sagarsaurus <sagrsccr@gmail.com>
2014-10-06 18:44:15 -0400

    Updated python files

--- Problem 5/hanoi_domain.py
+++ Problem 5/hanoi_domain.py
@@ -1,16 +1,16 @@
 import pyhop
 
 def move_disk(state, disk, below, new_below, source, dest):
-	if(state.disk[disk] == True &&
-	   state.on[disk] == below &&
-	   state.top[source] == disk &&
-	   state.top[dest] == new_below &&
-	   state.size[disk] < state.size[below] &&
-	   state.size[disk] < state.size[new_below] &&
-	   below != new_below &&
-	   disk != below &&
-	   disk != new_below &&
-	   source != dest)
+	if(state.disk[disk] == True and 
+	   state.on[disk] == below and
+	   state.top[source] == disk and
+	   state.top[dest] == new_below and
+	   state.size[disk] < state.size[below] and
+	   state.size[disk] < state.size[new_below] and
+	   below != new_below and
+	   disk != below and
+	   disk != new_below and
+	   source != dest):
 		state.on[disk] = new_below
 		state.top[source] = below
 		state.top[dest] = disk
@@ -21,7 +21,7 @@ pyhop.declare_operators(move_disk)
 
 def hanoi_sub(state, disk_size, source, dest, via):
 	disk = [item for item, size in state.size.items() if size == disk_size][0]
-	below = [below for top, below in state.on.items() if top = disk][0]
+	below = [below for top, below in state.on.items() if top == disk][0]
 	new_below = state.top[dest]
 	if(disk_size == 1):
 		return [('move_disk', disk, below, new_below, source, dest)]
