1a6cf5e051255cf333a86988dbe1809ee11062c2
james7132 <jliu348@gatech.edu>
2014-10-06 20:31:03 -0400

    Simplified states for HTN problems

--- Problem 5/hanoi_10.py
+++ Problem 5/hanoi_10.py
@@ -1,16 +1,8 @@
 from pyhop import *
-
 import hanoi_domain
 
 state = State('hanoi-state')
-state.disk = ['d1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 'd9', 'd10']
-state.clear = ['p1', 'p2', 'd1']
-state.size = {'d1':1, 'd2':2, 'd3':3, 'd4':4, 'd5':5, 'd6':6, 'd7':7, 'd8':8, 'd9':9, 'd10':10,
-			  'p1':11, 'p2':11, 'p3':11}
-state.on = {'d1':'d2', 'd2':'d3', 'd3':'d4', 'd4':'d5', 'd5':'d6', 'd6':'d7', 'd7':'d8', 'd8':'d9', 'd9':'d10', 'd10':'p3'}
+for i in range(1,11):
+	state.diskLocation[i] = 1
 
-goal = Goal('hanoi-goal')
-goal.on = {'d1':'d2', 'd2':'d3', 'd3':'d4', 'd4':'d5', 'd5':'d6', 'd6':'d7', 'd7':'d8', 'd8':'d9', 'd9':'d10', 'd10':'p1'}
-goal.clear = ['d1', 'p2', 'p3']
-
-pyhop(state, [('hanoi_sub', goal)], verbose=1)
+pyhop(state, [('move', 10, 1, 3)], verbose=1)
--- Problem 5/hanoi_domain.py
+++ Problem 5/hanoi_domain.py
@@ -1,30 +1,24 @@
 import pyhop
 
-def move_disk(state, disk, below, new_below):
-    print disk, below, new_below, source, dest, state.on[disk]
-    if(disk in state.disk and 
-       disk in state.clear and
-       new_below in state.clear and
-       state.size[disk] < state.size[new_below] and
-       below != new_below):
-    	state.on[disk] = new_below
-    	state.clear -= [new_below]
-    	state.clear += [below]
-    	return state
-    else:
-        return False
+def move_disk(state, disk, peg_source, peg_dest):
+	if state.diskLocation[disk] == peg_source:
+		state.diskLocation[disk] = peg_dest
+		return state
+	else:
+		return False
 
 pyhop.declare_operators(move_disk)
 
-def hanoi_sub(state, disk_size, source, dest, via):
-	disk = [item for item, size in state.size.items() if size == disk_size][0]
-	below = [below for top, below in state.on.items() if top == disk][0]
-	new_below = state.top[dest]
-	if(disk_size == 1):
-		return [('move_disk', disk, below, new_below, source, dest)]
+def move_multiple(state, disk, peg_source, peg_dest, peg_via):
+	if disk > 0:
+		return [('move', disk - 1, 	peg_source, peg_via, peg_dest), ('move_disk', disk, peg_source, peg_dest), ('move', disk - 1, peg_via, peg_dest, peg_source)]
 	else:
-		return [('hanoi_sub', disk_size - 1, source, via, dest), 
-				('move_disk', disk, below, new_below, source, dest),
-				('hanoi_sub', disk_size - 1, via, dest, source)]
-				
-pyhop.declare_methods('hanoi_sub', hanoi_sub)
+		return False
+		
+def move_single(state, disk , peg_source, peg_dest, peg_via):
+	if disk <= 0:
+		return [('move_disk', disk, peg_source, peg_dest)]
+	else:
+		return False
+		
+pyhop.declare_methods('move', move_single, move_multiple)
