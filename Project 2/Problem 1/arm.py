43173775860fa1a76b7fffe9507d9dad72d9aa1a
james7132 <james.siqi.liu@gmail.com>
2014-11-10 19:19:09 -0500

    Inverse Kinematics

--- Project 2/Problem 1/arm.py
+++ Project 2/Problem 1/arm.py
@@ -27,9 +27,19 @@ class RobotArm:
 		return len(r) != 0
 
 	def setQ(self, q):
+		self.q  = q 
 		self.a1 = Vector( self.l[0] * math.cos(q[0]), self.l[0] * math.sin(q[0]))
 		self.a2 = a1 + Vector( self.l[1] * math.cos(q[0] + q[1]), self.l[1] * math.sin(q[0] + q[1]))
 		self.a3 = a2 + Vector( self.l[2] * math.cos(q[0] + q[1] + q[2]), self.l[2] * math.sin(q[0] + q[1] + q[2]))
 		self.link1 = Line(Vector(0.0, 0.0), a1)
 		self.link2 = Line(a1, a2)
 		self.link3 = Line(a2, a3)
+
+	def inverseKinematics(self, x):
+		X2 = x(1) - self.l(3) * cos(x(3));      															#X2 = X - L3cos(theta)
+	    Y2 = x(2) - self.l(3) * sin(x(3));           														#Y2 = Y - L3cos(theta)
+	    det = X2**2 + Y2**2;                      															#det = X2 ^ 2 + Y2 ^ 2
+	    Q1 = acos(X2/sqrt(det)) + acos((self.l(1)**2 - self.l(2)**2 + det) / (2 * self.l(1) * sqrt(det)));  #Law of Cosines
+	    Q2 = pi + acos((self.l(1)**2 + self.l(2)**2 - det)/(2 * self.l(1) * self.l(2)));					#Law of Cosines
+	    Q3 = x(3) - Q1 - Q2;                  														 		#theta3 = theta - theta1 - theta2
+	    return [Q1, Q2, Q3];      			  																#CaLcuLate q_i from X
